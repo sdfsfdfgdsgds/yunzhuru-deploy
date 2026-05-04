4dc402639e1466b76dc985da412152a6bc4bc6c2
sdfsfdfgdsgds <laosijiapps@gmail.com>
2026-05-04 13:21:34 +0800

    fix db config

--- config/db.php
+++ config/db.php
@@ -1,8 +1,9 @@
 <?php
-$port = 33070;
-$dsn = "mysql:host=127.0.0.1;port={$port};dbname=yunzhuru;charset=utf8mb4";
-$username = 'root';
-$password = 'Yyf@Mysql2026!';
+$host = getenv("DB_HOST") ?: "143.92.40.164";
+$port = intval(getenv("DB_PORT") ?: 33061);
+$dsn = "mysql:host={$host};port={$port};dbname=yunzhuru;charset=utf8mb4";
+$username = getenv("DB_USER") ?: "root";
+$password = getenv("DB_PASS") ?: "Yyf@Mysql2026!";
 try {
     $pdo = new PDO($dsn, $username, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
